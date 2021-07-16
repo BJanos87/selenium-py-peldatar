854b3dce7cdae7ec08d6d91acc731a5520bb26ab
Janos Babarik <babarik.janos@gmail.com>
2021-07-16 22:46:27 +0200

    python2-homework/charsandord.py file is modified

--- python2-homework/charsandord.py
+++ python2-homework/charsandord.py
@@ -7,9 +7,10 @@ words_presence = {}
 # Beolvasom a TEXT.txt file-t (nyers sorokat listában kapom meg)
 with open("TEXT.txt", 'r') as file:
     text = (file.readlines())
+    file.close()
 
 # Az így kapott sorok listáján végigiterálok, kitörlöm a nem szükséges karaktereket, illetve kisbetűssé konvertálom
-# majd a már formázott sorokat felfűzöm egy nagy string-é
+# majd a már formázott sorokat felfűzöm egy hosszú string-é
 for row in text:
     for character in rm_characters:
         row = row.replace(character, "").lower()
@@ -20,13 +21,15 @@ text_list = text_modded.split()
 text_list.sort()
 
 # Az így kapott szavak listáján végigiterálok, és előfordulás szerint egy szótárba jegyzem be azokat
-# {szó : előfurdolás száma}
+# {talált_szó : előfurdolás száma}
 for word in text_list:
-    if not word in words_presence:
+    if word not in words_presence:
         words_presence[word] = 1
     else:
         words_presence[word] += 1
 
 # Az így kapott szótárat string-ként kiírom egy txt fájlba
-with open("words_presence.txt", 'w') as new:
-    new.write(str(words_presence))
+with open("words_presence.txt", 'w') as new_file:
+    for word in words_presence:
+        new_file.write(f'{word} : {str(words_presence[word])} \n')
+    new_file.close()
