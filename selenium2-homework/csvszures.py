59536a2fb122315f0dbd5567ed4648d53e3a85b9
Janos Babarik <babarik.janos@gmail.com>
2021-07-17 02:10:06 +0200

    selenium2-homework/csvszures.py file is modified

--- selenium2-homework/csvszures.py
+++ selenium2-homework/csvszures.py
@@ -9,7 +9,7 @@ with open ("table_in.csv") as csv_file:
     csv_file.close()
 
 with open ("table_in_other.csv", "w") as csv_file:
-    write_file = csv.writer(csv_file)
+    write_file = csv.writer(csv_file, delimiter=',')
     write_file.writerows(filtered_list)
     csv_file.close()
 
