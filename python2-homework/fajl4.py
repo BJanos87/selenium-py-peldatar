423b0a97ea9d20e3c3ed04e6b3d5b4e88d368662
Janos Babarik <babarik.janos@gmail.com>
2021-07-18 16:57:59 +0200

    python2-homework/fajl4.py file is modified

--- python2-homework/fajl4.py
+++ python2-homework/fajl4.py
@@ -2,6 +2,6 @@ with open("adat.txt", "r") as file:
     rows = file.readlines()
     file.close()
 with open("fajl4_other.txt", "w") as file_other:
-    for word in rows:
-        file_other.write(word)
+    for row in rows:
+        file_other.write(row)
     file_other.close()
